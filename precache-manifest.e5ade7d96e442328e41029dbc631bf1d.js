d1a0fcd4c991fe7eac9be1e0f1142d5a22df6acf
Lucas Becker <lucasbecker451@gmail.com>
2022-08-12 21:23:13 -0400

    Updates

---
+++ precache-manifest.e5ade7d96e442328e41029dbc631bf1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb60de45949d890a589e",
+    "url": "/react-meetups-app/static/css/main.54003a07.chunk.css"
+  },
+  {
+    "revision": "eb60de45949d890a589e",
+    "url": "/react-meetups-app/static/js/main.eb60de45.chunk.js"
+  },
+  {
+    "revision": "40482ff182afd0c7577b",
+    "url": "/react-meetups-app/static/js/1.40482ff1.chunk.js"
+  },
+  {
+    "revision": "5117753cf90bfcaf406a",
+    "url": "/react-meetups-app/static/js/runtime~main.5117753c.js"
+  },
+  {
+    "revision": "451dd5aa76da21dc40043ebdff2d5f53",
+    "url": "/react-meetups-app/index.html"
+  }
+];
